b7084eeb400173a24a55270de2202a4aac2b6ee9
clanie1 <danybarociov@gmail.com>
2024-05-11 16:41:58 -0700

    mqtt

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
 import supabase from "./data/supabase";
+import mqtt from "mqtt";
 dotenv.config();
+
+const client = mqtt.connect("mqtt://mqtt.eclipseprojects.io");
+
+client.on("connect", function () {
+  client.subscribe("iot-cetys/cat");
+});
+
+client.on("error", function (error) {
+  console.error("MQTT client error: ", error);
+});
+
+let entrada: Date | null = null;
+
+client.on("message", function (topic, message) {
+  const mssg = message.toString();
+  console.log(topic, message.toString());
+  if (mssg == "ENTRAR") {
+    entrada = new Date();
+  }
+  if (mssg == "SALIR" && entrada != null) {
+    const salida = new Date();
+    insertRecord({ started_at: entrada, ended_at: salida });
+    entrada = null;
+  }
+});
+
+const insertRecord = async (obj: any) => {
+  let cosa = await supabase.from("records").insert(obj);
+};
 
 const app = express();
 const port = process.env.PORT;
